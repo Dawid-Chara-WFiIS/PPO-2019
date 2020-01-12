e948c49437d8c35a24af92d39f34716a72694cfc
Dawid Chara <dawid.ch.9@o2.pl>
2020-01-12 22:18:47 +0100

    lab11 added

---
+++ Lab11/build/CMakeFiles/box.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dawid/PPO2019/Lab11/Main.cpp" "/home/dawid/PPO2019/Lab11/build/CMakeFiles/box.dir/Main.cpp.o"
+  "/home/dawid/PPO2019/Lab11/src/BContainer.cpp" "/home/dawid/PPO2019/Lab11/build/CMakeFiles/box.dir/src/BContainer.cpp.o"
+  "/home/dawid/PPO2019/Lab11/src/Box.cpp" "/home/dawid/PPO2019/Lab11/build/CMakeFiles/box.dir/src/Box.cpp.o"
+  "/home/dawid/PPO2019/Lab11/src/Volume.cpp" "/home/dawid/PPO2019/Lab11/build/CMakeFiles/box.dir/src/Volume.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab11/build/CMakeFiles/box.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/box.dir/Main.cpp.o"
+  "CMakeFiles/box.dir/src/BContainer.cpp.o"
+  "CMakeFiles/box.dir/src/Box.cpp.o"
+  "CMakeFiles/box.dir/src/Volume.cpp.o"
+  "box.pdb"
+  "box"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/box.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
